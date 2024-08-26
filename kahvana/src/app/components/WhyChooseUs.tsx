dfe61812f8f8c5fe78bd300470a91979a7c154bc
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-08-26 15:36:35 +0200

    creating new section

---
+++ kahvana/src/app/components/WhyChooseUs.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function WhyChooseUs() {
+  return (
+    <div>
+        <div className='flex text-white mt-32'>
+            <div className='w-475 h-0.5 bg-white'></div>
+            <h2>Why choose us?</h2>
+            <div className='w-475 h-0.5 bg-white'></div>
+         </div>
+       
+
+      
+    </div>
+  )
+}
+
+export default WhyChooseUs
--- kahvana/src/app/page.tsx
+++ kahvana/src/app/page.tsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Services from "./components/Services";
+import WhyChooseUs from "./components/WhyChooseUs";
 
 export default function Page() {
   return (
@@ -11,6 +12,7 @@ export default function Page() {
       <Navbar></Navbar>
       <Home></Home>
       <Services></Services>
+      <WhyChooseUs></WhyChooseUs>
       
 
     </main>
