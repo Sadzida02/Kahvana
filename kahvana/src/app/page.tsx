7d7544a8645fe6098cdacc66076395b09abbefb9
Sadžida Valjevac <valjevac.sadzida02@gmail.com>
2024-08-31 21:37:35 +0200

    creating trusted2

--- kahvana/src/app/page.tsx
+++ kahvana/src/app/page.tsx
@@ -9,6 +9,7 @@ import Trusted from "./components/Trusted";
 import Form from "./components/Form";
 import Footer from "./components/Footer";
 import Hvala from "./components/Hvala";
+import Trusted2 from "./components/Trusted2";
 
 export default function Page() {
   return (
@@ -19,6 +20,7 @@ export default function Page() {
       <Services></Services>
       <WhyChooseUs></WhyChooseUs>
       <Trusted></Trusted>
+      <Trusted2></Trusted2>
       <Services2></Services2>
       <Form></Form>
       <Hvala></Hvala>
