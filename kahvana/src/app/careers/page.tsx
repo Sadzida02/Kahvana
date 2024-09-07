26a25c8d1dd7ef9855198cc2ee69aafa476320f2
SakicHarun <harun.sakic2019@size.ba>
2024-09-07 15:11:13 +0200

    napravljena je sekcija careers

---
+++ kahvana/src/app/careers/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Careers from '../components/Careers'
+import Footer from '../components/Footer'
+
+const page = () => {
+  return (
+    <div>
+    <Navbar/>
+    <Careers/>
+    <Footer/>  
+    </div>
+  )
+}
+
+export default page
---
+++ kahvana/src/app/mobileDeveloperApplication/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MobileDeveloperApplication from '../components/MobileDeveloperApplication'
+function page() {
+  return (
+    <div>
+      <MobileDeveloperApplication></MobileDeveloperApplication>
+    </div>
+  )
+}
+
+export default page
